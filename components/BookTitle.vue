1b57d2db61a2fa84253ad20b96edc42616d11af2
fortune-max <fortmax120@gmail.com>
2023-01-16 05:35:49 +0100

    trim book titles

--- components/BookTitle.vue
+++ components/BookTitle.vue
@@ -3,7 +3,7 @@
     const title = computed(() => {
         const startIdx = props.goodreadsID.match('-|\\.').index + 1
         let title: string = props.goodreadsID.substring(startIdx)
-        title = title.replaceAll('-', ' ').replaceAll('_', ' ')
+        title = title.replaceAll('-', ' ').replaceAll('_', ' ').trim()
         title = title.split(' ').map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
         return title
     })
