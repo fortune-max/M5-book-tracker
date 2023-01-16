41fc233a430b145d1653365a67241bbdaac34599
fortune-max <fortmax120@gmail.com>
2023-01-16 06:53:14 +0100

    rename path

---
+++ pages/Shelves/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const yearList = [
+        { name: 'Favorites', url: 'shelves/5star'},
+        { name: 'Before I Die - Unread', url: 'shelves/bid_unread'},
+        { name: 'Before I Die - Read', url: 'shelves/bid_read'}
+    ]
+</script>
+
+<template>
+    <div class="flex content-center flex-col">
+        <ListRenderer :listItems="yearList" />
+    </div>
+    
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+
+import ListItem from '~~/components/ListItem.vue';
+
+
+import AlertBox from '~~/components/AlertBox.vue';
+
+<template>
+    <h1>Hello world</h1>
+</template>
