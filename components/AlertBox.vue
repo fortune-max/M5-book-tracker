76febe51f8a9676cf59a59d8b1c893f9f9269dc2
fortune-max <fortmax120@gmail.com>
2023-01-16 05:36:06 +0100

    add AlertBox

---
+++ components/AlertBox.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+    type AlertBoxTypes = "error" | "success" | "info" | "warning";
+    const props = defineProps<{
+        type: AlertBoxTypes
+    }>();
+
+    const computedClasses = computed(() => {
+        switch (props.type) {
+            case "error":
+                return "red";
+            case "warning":
+                return "orange";
+            case "info":
+                return "blue";
+            case "success":
+                return "green";
+        }
+    });
+</script>
+
+<template>
+    <div :class="computedClasses">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+.red {  /* error */
+    color: red;
+}
+
+.blue { /* default */
+    color: blue;
+}
+
+.success { /* success */
+    color: green;
+}
+
+.orange {   /* warning */
+    color: orange;
+}
+
+.green { /* success */
+    color: green;
+}
+
+</style>
