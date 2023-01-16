34830faa138971b34c0a51bf998dcecd70197845
fortune-max <fortmax120@gmail.com>
2023-01-16 06:34:27 +0100

    add ListRenderer

---
+++ components/ListItem.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const props = defineProps(["listItem"]);
+</script>
+
+<template>
+    <span class="border-solid border-2 border-b-green-400 rounded-sm px-4 truncate w-64 text-center inline-block">
+        <a :href="props.listItem.url">
+            {{ props.listItem.name }}
+        </a>
+    </span>
+</template>
---
+++ components/ListRenderer.vue
@@ -0,0 +1,9 @@
+<script setup>
+    const props = defineProps(["listItems"]);
+</script>
+
+<template>
+    <div class="flex flex-col gap-2">
+        <ListItem v-for="listItem in props.listItems" :listItem="listItem" />
+    </div>
+</template>
