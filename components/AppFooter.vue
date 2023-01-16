0dcc0a9d8aafd9dd2a397f3f4a3a62ad3ae4fd74
fortune-max <fortmax120@gmail.com>
2023-01-16 07:18:31 +0100

    update header and footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div
+      class="text-center p-3"
+      style="background-color: rgba(0, 0, 0, 0.2)"
+    >
+      © 2023 Copyright:
+      <a class="text-dark" href="https://Greatreads.com/"
+        >Greatreads.com</a
+      >
+    </div>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <NuxtLink to="/">
+            <img src="../public/logo.jpg" alt="logo" class="h-32 w-32" />
+        </NuxtLink>
+    
+        <nav>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/year-in-books">Year in Books</NuxtLink>
+            <NuxtLink to="/shelves">Shelves</NuxtLink>
+            <NuxtLink to="/about">About</NuxtLink>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+div {
+    background-color: #d9bca9;
+}
+
+nav {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
