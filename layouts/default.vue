2b9b0cd284bd93cdc6b49101dfc694e33dd48288
fortune-max <fortmax120@gmail.com>
2023-01-16 07:17:57 +0100

    add layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
-    <AppHeader/>
-        <slot />
-    <AppFooter/>
+    <div class="page-wrapper">
+        <AppHeader/>
+        <main>
+            <slot/>
+        </main>
+        <AppFooter/>
+    </div>
 </template>
+
+<style scoped>
+.page-wrapper {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
+
+main {
+    padding: 20px;
+    background-color: cadetblue;
+    height: inherit;
+    flex-grow: 1;
+}
+</style>
--- layouts/no-footer.vue
+++ layouts/no-footer.vue
@@ -1,4 +1,23 @@
 <template>
-    <AppHeader/>
-        <slot />
+    <div class="page-wrapper">
+        <AppHeader/>
+        <main>
+            <slot/>
+        </main>
+    </div>
 </template>
+
+<style scoped>
+.page-wrapper {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
+
+main {
+    padding: 20px;
+    background-color: cadetblue;
+    height: inherit;
+    flex-grow: 1;
+}
+</style>
