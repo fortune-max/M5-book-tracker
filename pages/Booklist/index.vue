2b5376e72d7ed0ffda62d4fd98dadaed652dfbca
fortune-max <fortmax120@gmail.com>
2023-01-16 06:35:09 +0100

    add Book Categories page

---
+++ pages/Booklist/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const yearList = [
+        { name: 'Favorites', url: 'booklist/5star'},
+        { name: 'Before I Die - Unread', url: 'booklist/bid_unread'},
+        { name: 'Before I Die - Read', url: 'booklist/bid_read'}
+    ]
+</script>
+
+<template>
+    <div class="m-auto">
+        <ListRenderer :listItems="yearList" />
+    </div>
+</template>
---
+++ pages/Year-In-Books/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const yearList = [
+        { name: '2017', url: 'year-in-books/2017'},
+        { name: '2018', url: 'year-in-books/2018'},
+        { name: '2019', url: 'year-in-books/2019'},
+        { name: '2020', url: 'year-in-books/2020'},
+        { name: '2021', url: 'year-in-books/2021'},
+        { name: '2022', url: 'year-in-books/2022'},
+    ]
+</script>
+
+<template>
+    <div class="m-auto">
+        <ListRenderer :listItems="yearList" />
+    </div>
+</template>
