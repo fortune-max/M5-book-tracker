8b43ae5bab9480e2bdbaaedeaa21c065e90d4039
fortune-max <fortmax120@gmail.com>
2023-01-17 22:00:09 +0100

    update AppHeader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,20 +1,24 @@
 <template>
     <div>
-        <NuxtLink to="/">
-            <img src="../public/logo.jpg" alt="logo" class="h-32 w-32" />
-        </NuxtLink>
+        <div id="header-container" class="flex items-center justify-between">
+            <NuxtLink to="/" class="inline-block mx-12">
+                <img src="../public/logo.jpg" alt="logo" class="h-28 w-28" />
+            </NuxtLink>
+            <LoginButton class="mx-12"/>
+        </div>
     
         <nav>
             <NuxtLink to="/">Home</NuxtLink>
             <NuxtLink to="/year-in-books">Year in Books</NuxtLink>
             <NuxtLink to="/shelves">Shelves</NuxtLink>
             <NuxtLink to="/about">About</NuxtLink>
+            <NuxtLink to="/members">Members</NuxtLink>
         </nav>
     </div>
 </template>
 
 <style scoped>
-div {
+#header-container, nav {
     background-color: #d9bca9;
 }
 
