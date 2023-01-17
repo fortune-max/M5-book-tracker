c025ceedda7862bae7a085cd4d0998e0a2292e96
fortune-max <fortmax120@gmail.com>
2023-01-17 22:02:36 +0100

    add members page

---
+++ pages/members.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { storeToRefs } from 'pinia';
+    import {useUserStore} from '~~/stores/userStore';
+    
+    definePageMeta({
+        validate: async (route) => {
+            const userStore = useUserStore();
+            const { isLoggedIn } = storeToRefs(userStore);
+            if (!isLoggedIn.value) {
+                return {statusCode: 401, message: 'You are not logged in'}
+            }
+            return true;
+        }
+    });
+</script>
+<template>
+    This is the members Area - you are logged in!
+</template>
