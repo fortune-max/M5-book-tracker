1b2ae10fdef430e42687ebf9f1bf470c20b1ffce
fortune-max <fortmax120@gmail.com>
2023-01-17 11:46:43 +0100

    update footer background color

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div
       class="text-center p-3"
-      style="background-color: rgba(0, 0, 0, 0.2)"
+      style="background-color: #588157"
     >
       © 2023 Copyright:
       <a class="text-dark" href="https://Greatreads.com/"
